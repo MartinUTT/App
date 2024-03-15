e9224347ffa5d3073c9150c47e5cced84dbef97e
MartinUTT <158416088+MartinUTT@users.noreply.github.com>
2024-03-14 22:51:30 -0700

    Add files via upload

---
+++ Portafolio 2/navigation/navigation/MainStack.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Profile from "../screens/Profile";
+import Home from "../screens/Home";
+
+const Stack = createNativeStackNavigator()
+
+export default function MainStack() {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+
+                <Stack.Screen name='Home' component = {Home} />
+                <Stack.Screen name='Profile' component = {Profile} />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );   
+}
+
